e0cfbb72a5a7ec9ba8c19ea7fc075fa8e01fed5f
Juan Pedro Pont Verges <pedro@cacao.to>
2022-11-03 19:25:02 -0300

    chagned reserved env variables

--- src/pages/api/upload-url.ts
+++ src/pages/api/upload-url.ts
@@ -7,12 +7,12 @@ export default async function handler(
 ) {
   const s3 = new S3({
     apiVersion: '2006-03-01',
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.WAPI_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.WAPI_AWS_SECRET_ACCESS_KEY,
   })
 
   const post = await s3.createPresignedPost({
-    Bucket: process.env.AWS_PUBLIC_BUCKET_NAME,
+    Bucket: process.env.WAPI_AWS_PUBLIC_BUCKET_NAME,
     Fields: {
       key: req.query.file,
       'Content-Type': req.query.fileType,
